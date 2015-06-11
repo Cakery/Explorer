5833b0743b1e8e04a4ba869bbde33a2a66557979
Cake <gogomilo@hotmail.com>
2015-06-11 21:39:09 +1000

    Updates to the Dark level, including fading in text

---
+++ Assets/Scripts/LevelSwitch.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSwitch : MonoBehaviour {
+	public int SceneID;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter (Collider Collider) {
+	if (Collider.tag == "Player") {
+						if (SceneID == 0) {
+								Application.LoadLevel (Application.loadedLevel + 1);
+						} else {
+				Application.LoadLevel (SceneID);
+			}
+				}
+	}
+}
---
+++ Assets/Scripts/TextController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextController : MonoBehaviour {
+	Color Alpha;
+
+	bool FadedIn;
+	bool FadedOut;
+	// Use this for initialization
+	void Start () {
+		Alpha= guiText.material.color;
+		Alpha.a = 0.0F;
+		guiText.material.color = Alpha;
+
+		//StartCoroutine (FadeIn ());
+	}
+
+	IEnumerator FadeIn() {
+		while (Alpha.a<1) {
+			//print(Alpha.a);
+			Alpha.a+=0.01F*Time.deltaTime*1.1F;
+			guiText.material.color = Alpha;
+			yield return new WaitForSeconds(0.1F);
+		}
+		FadedIn = true;
+	}
+
+
+	IEnumerator FadeOut() {
+		while (Alpha.a>0) {
+
+			Alpha.a-=0.01F*Time.deltaTime*1.1F;
+			guiText.material.color = Alpha;
+			//print(Alpha.a);
+			yield return new WaitForSeconds(0.1F);
+
+		}
+		FadedOut = true;
+	}
+	// Update is called once per frame
+	void Update () {
+				if (!FadedIn) {
+						StartCoroutine (FadeIn ());
+				} else {
+						StartCoroutine (FadeOut ());
+			if (FadedOut) {
+				this.gameObject.SetActive(false);
+			}
+				}
+		}
+}
