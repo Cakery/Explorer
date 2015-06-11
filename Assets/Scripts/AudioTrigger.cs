0326c001db6010c61354d1b2f0e77cb3dc3b6f13
Cake <gogomilo@hotmail.com>
2015-06-11 16:43:47 +1000

    New audio related ambient things for the second level

---
+++ Assets/Scripts/AudioTrigger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioTrigger : MonoBehaviour {
+	public bool PlayOnce;
+
+	void OnTriggerEnter() {
+		if (!this.audio.isPlaying) {
+						if (!PlayOnce) {
+								this.audio.Play ();
+								
+						}
+				}
+	}
+}
