c3b83a3d6388cfa9d0db03f7edc0a53cc7ac2411
Cake <gogomilo@hotmail.com>
2015-06-13 19:33:48 +1000

    Added the basics of the new UI menu

---
+++ Assets/SliderController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SliderController : MonoBehaviour {
+	Slider Slider;
+	public InputField Text;
+	// Use this for initialization
+	void Start () {
+		Slider = GetComponent<Slider>();
+	}
+	public void TextChangeValue() {
+				float Num = float.Parse (Text.text);
+				if (Num >= Slider.minValue & Num <= Slider.maxValue) {
+						Slider.value = Num;
+				}
+		}
+	// Update is called once per frame
+	public void SliderChangeValue() {
+		print (Slider.value);
+		Text.text = Slider.value.ToString();
+	}
+}
